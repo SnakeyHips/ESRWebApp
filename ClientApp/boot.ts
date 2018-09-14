04227b39f0889a96e61c09d2ce77bb8cd1847e70
Jake Gough <super.jake@hotmail.co.uk>
2018-09-14 13:43:54 +0100

    Update boot.ts

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -5,17 +5,18 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: require('./components/home/home.vue.html').default },
-		{ path: '/fetchemployee', component: require('./components/employee/fetchemployee.vue.html').default },
-		{ path: '/createemployee', component: require('./components/employee/createemployee.vue.html').default },
-		{ path: '/editemployee/:id', component: require('./components/employee/editemployee.vue.html').default },
-		{ path: '/fetchsession', component: require('./components/session/fetchsession.vue.html').default },
-		{ path: '/createsession', component: require('./components/session/createsession.vue.html').default },
-		{ path: '/editsession/:id', component: require('./components/session/editsession.vue.html').default },
-		{ path: '/rostersession/:id', component: require('./components/session/rostersession.vue.html').default },
-		{ path: '/fetchabsence', component: require('./components/absence/fetchabsence.vue.html').default },
-		{ path: '/createabsence', component: require('./components/absence/createabsence.vue.html').default },
-		{ path: '/editabsence/:id', component: require('./components/absence/editabsence.vue.html').default },
+	{ path: '/', component: require('./components/home/home.vue.html').default },
+	{ path: '/fetchsession', component: require('./components/session/fetchsession.vue.html').default },
+	{ path: '/createsession', component: require('./components/session/createsession.vue.html').default },
+	{ path: '/editsession/:id', component: require('./components/session/editsession.vue.html').default },
+	{ path: '/rostersession/:id', component: require('./components/session/rostersession.vue.html').default },
+	{ path: '/fetchemployee', component: require('./components/employee/fetchemployee.vue.html').default },
+	{ path: '/createemployee', component: require('./components/employee/createemployee.vue.html').default },
+	{ path: '/editemployee/:id', component: require('./components/employee/editemployee.vue.html').default },
+	{ path: '/fetchroster', component: require('./components/roster/fetchroster.vue.html').default },
+	{ path: '/fetchabsence', component: require('./components/absence/fetchabsence.vue.html').default },
+	{ path: '/createabsence', component: require('./components/absence/createabsence.vue.html').default },
+	{ path: '/editabsence/:id', component: require('./components/absence/editabsence.vue.html').default },
 ];
 
 new Vue({
