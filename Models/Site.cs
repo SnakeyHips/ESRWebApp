feda273608242102ac38ae532648ce0ead5df521
Jake Gough <super.jake@hotmail.co.uk>
2018-09-17 09:45:28 +0100

    Update Site.cs

--- Models/Site.cs
+++ Models/Site.cs
@@ -7,6 +7,7 @@ namespace ERSWebApp.Models
 {
     public class Site
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Times { get; set; }
