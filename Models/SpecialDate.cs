f1cb0d42173a34a1fe6f81f804769619cd1037ea
Jake Gough <super.jake@hotmail.co.uk>
2018-09-17 09:45:51 +0100

    Create SpecialDate.cs

---
+++ Models/SpecialDate.cs
@@ -0,0 +1,9 @@
+namespace ERSWebApp.Models
+{
+    public class SpecialDate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+    }
+}
