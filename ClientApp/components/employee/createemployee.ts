4633134e379242ab73f1d7b037d9c17d5eef00b4
Jake Gough <super.jake@hotmail.co.uk>
2018-09-18 14:16:24 +0100

    Update createemployee.ts

--- ClientApp/components/employee/createemployee.ts
+++ ClientApp/components/employee/createemployee.ts
@@ -1,9 +1,18 @@
-﻿import Vue from 'vue';
+import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import { Employee } from '../../models/employee';
 
 @Component
 export default class CreateEmployeeComponent extends Vue {	
+	$refs!: {
+		form: HTMLFormElement
+	}
+
+	rules: object = {
+		required: value => !!value || 'Required',
+		number: value => /[0-9]/.test(value) || 'Value must be number e.g. "8" or "10"',
+		decimal: value => /^\d+(\.\d{1,2})?$/.test(value) || 'Value must be decimal e.g. "8.0" or "7.5"'
+	}
 
 	employee: Employee = {
 		id: 0,
@@ -21,24 +30,39 @@ export default class CreateEmployeeComponent extends Vue {
 		workPattern: "",
 		status: ""
 	}
+
+	failed: boolean = false;
 	workpattern: string[] = [];
+	roles: string[] = ["SV", "DRI", "RN", "CCA"];
+	days: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 	createEmployee() {
-		//Get work pattern first
-		this.employee.workPattern = this.workpattern.join();
+		this.failed = false;
+		if (this.$refs.form.validate()){
+			//Get work pattern first
+			this.employee.workPattern = this.workpattern.join();
 
-		//Then send employee to backend
-		fetch('api/Employee/Create', {
-			method: 'POST',
-			body: JSON.stringify(this.employee)
-		})
-			.then(response => response.json() as Promise<number>)
-			.then(data => {
-				if (data < 1) {
-					alert("Failed to create Employee. Please make sure the Id is not already in use.");
-				} else {
-					this.$router.push('/fetchemployee');
-				}
+			//Then send employee to backend
+			fetch('api/Employee/Create', {
+				method: 'POST',
+				body: JSON.stringify(this.employee)
 			})
+				.then(response => response.json() as Promise<number>)
+				.then(data => {
+					if (data < 1) {
+						this.failed = true;
+					} else {
+						this.$router.push('/fetchemployee');
+					}
+				})
+		}
+	}
+
+	clear() {
+		this.$refs.form.reset();
+	}
+
+	cancel() {
+		this.$router.push('/fetchemployee');
 	}
 }
