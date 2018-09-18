41b6830a7aafeb4bc795ad6495dd169f8842927b
Jake Gough <super.jake@hotmail.co.uk>
2018-09-18 14:14:40 +0100

    Update editsession.ts

--- ClientApp/components/session/editsession.ts
+++ ClientApp/components/session/editsession.ts
@@ -1,24 +1,113 @@
-<template>
-	<v-container>
-		<h1>Create Session</h1>
-		<v-form ref="form" lazy-validation>
-			<v-menu :nudge-right="40" lazy transition="scale-transition" offset-y >
-				<v-text-field slot="activator" v-model="session.date" label="Session date" append-icon="event" :rules="[rules.required]" readonly required></v-text-field>
-				<v-date-picker v-model="session.date"></v-date-picker>
-			</v-menu>
-			<v-select v-model="session.type" :items="types" label="Type" v-on:change="loadSites(session.type)" :rules="[rules.required]" requried />
-			<v-select v-model="session.site" :items="sites" label="Site" name="session.site" item-text="name" v-on:change="loadTimes(session.site)" :rules="[rules.required]" requried />
-			<v-select v-model="session.time" :items="times" label="Clinic Time" :rules="[rules.required]" requried />
-			<v-text-field v-model="session.lod" label="LOD" :rules="[rules.required, rules.decimal]" maxlength="5" required />
-			<v-text-field v-model="session.chairs" label="Chairs" :rules="[rules.required, rules.number]" maxlength="2" required />
-			<v-text-field v-model="session.occ" label="OCC" :rules="[rules.required, rules.number]" maxlength="2" required />
-			<v-text-field v-model="session.estimate" label="Estimate" :rules="[rules.required, rules.number]"maxlength="2"  required />
-			<v-btn v-on:click="createSession">Submit</v-btn>
-			<v-btn v-on:click="clear">Clear</v-btn>
-			<v-btn v-on:click="cancel">Cancel</v-btn>
-			<v-alert :value="failed" type="error">Failed to create session!</v-alert>
-		</v-form>
-	</v-container>
-</template>
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { Session } from '../../models/session';
 
-<script src="./createsession.ts"></script>
+@Component
+export default class EditSessionComponent extends Vue {	
+	$refs!: {
+		form: HTMLFormElement
+	}
+
+	rules: object = {
+		required: value => !!value || 'Required',
+		number: value => /[0-9]/.test(value) || 'Value must be number e.g. "8" or "10"',
+		decimal: value => /^\d+(\.\d{1,2})?$/.test(value) || 'Value must be decimal e.g. "8.0" or "7.5"'
+	}
+
+	session: Session = {
+		id: 0,
+		date: "",
+		day: "",
+		type: "",
+		site: "",
+		time: "",
+		lod: 0,
+		chairs: 0,
+		occ: 0,
+		estimate: 0,
+		holiday: 0,
+		note: "",
+		sV1Id: 0,
+		sV1Name: "",
+		sV1LOD: 0,
+		sV1UNS: 0,
+		sV1OT: 0,
+		drI1Id: 0,
+		drI1Name: "",
+		drI1LOD: 0,
+		drI1UNS: 0,
+		drI1OT: 0,
+		drI2Id: 0,
+		drI2Name: "",
+		drI2LOD: 0,
+		drI2UNS: 0,
+		drI2OT: 0,
+		rN1Id: 0,
+		rN1Name: "",
+		rN1LOD: 0,
+		rN1UNS: 0,
+		rN1OT: 0,
+		rN2Id: 0,
+		rN2Name: "",
+		rN2LOD: 0,
+		rN2UNS: 0,
+		rN2OT: 0,
+		rN3Id: 0,
+		rN3Name: "",
+		rN3LOD: 0,
+		rN3UNS: 0,
+		rN3OT: 0,
+		ccA1Id: 0,
+		ccA1Name: "",
+		ccA1LOD: 0,
+		ccA1UNS: 0,
+		ccA1OT: 0,
+		ccA2Id: 0,
+		ccA2Name: "",
+		ccA2LOD: 0,
+		ccA2UNS: 0,
+		ccA2OT: 0,
+		ccA3Id: 0,
+		ccA3Name: "",
+		ccA3LOD: 0,
+		ccA3UNS: 0,
+		ccA3OT: 0,
+		staffCount: 0,
+		state: 0
+	}
+
+	loading: boolean = false;
+	failed: boolean = false;
+
+	mounted() {
+		this.loading = true;
+		fetch('api/Session/GetById?id=' + this.$route.params.id)
+			.then(respone => respone.json() as Promise<Session>)
+			.then(data => {
+				this.session = data;
+				this.loading = false;
+			});
+	}
+
+	editSession() {
+		this.failed = false;
+		if (this.$refs.form.validate()){
+			fetch('api/Session/Update', {
+				method: 'PUT',
+				body: JSON.stringify(this.session)
+			})
+				.then(response => response.json() as Promise<number>)
+				.then(data => {
+					if (data < 1) {
+						this.failed = true;
+					} else {
+						this.$router.push('/fetchsession');
+					}
+				})
+		}
+	}
+
+	cancel() {
+		this.$router.push('/fetchsession');
+	}
+}
