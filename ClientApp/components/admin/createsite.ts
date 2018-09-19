cc6bf19b04202defeb57318f12654183e2956f2d
Jake Gough <super.jake@hotmail.co.uk>
2018-09-19 10:57:03 +0100

    Update createsite.ts

--- ClientApp/components/admin/createsite.ts
+++ ClientApp/components/admin/createsite.ts
@@ -1,9 +1,18 @@
-﻿import Vue from 'vue';
+import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import { Site } from '../../models/site';
 
 @Component
 export default class CreateSiteComponent extends Vue {	
+	$refs!: {
+		form: HTMLFormElement
+	}
+
+	rules: object = {
+		required: value => !!value || 'Required',
+		number: value => /[0-9]/.test(value) || 'Value must be number e.g. "8" or "10"',
+		decimal: value => /^\d+(\.\d{1,2})?$/.test(value) || 'Value must be decimal e.g. "8.0" or "7.5"'
+	}
 
 	site: Site = {
 		id: 0,
@@ -12,18 +21,32 @@ export default class CreateSiteComponent extends Vue {
 		times: ""
 	}
 
+	failed: boolean = false;
+	types: string[] = ["Community", "MDC"];
+
 	createSite() {
-		fetch('api/Admin/CreateSite', {
-			method: 'POST',
-			body: JSON.stringify(this.site)
-		})
-			.then(response => response.json() as Promise<number>)
-			.then(data => {
-				if (data < 1) {
-					alert("Failed to create Site. Please make sure all of the fields are completed.");
-				} else {
-					this.$router.push('/fetchadmin');
-				}
+		this.failed = false;
+		if (this.$refs.form.validate()) {
+			fetch('api/Admin/CreateSite', {
+				method: 'POST',
+				body: JSON.stringify(this.site)
 			})
+				.then(response => response.json() as Promise<number>)
+				.then(data => {
+					if (data < 1) {
+						this.failed = true;
+					} else {
+						this.$router.push('/fetchadmin');
+					}
+				})
+		}
+	}
+
+	clear() {
+		this.$refs.form.reset();
+	}
+
+	cancel() {
+		this.$router.push('/fetchadmin');
 	}
 }
