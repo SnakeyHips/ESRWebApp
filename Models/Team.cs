6cd8d1f8274ba486c84ca80c2a6bee207ca126e5
Jake Gough <super.jake@hotmail.co.uk>
2018-09-21 11:55:46 +0100

    Create Team.cs

---
+++ Models/Team.cs
@@ -0,0 +1,26 @@
+namespace ERSWebApp.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SV1Id { get; set; }
+        public string SV1Name { get; set; }
+        public int DRI1Id { get; set; }
+        public string DRI1Name { get; set; }
+        public int DRI2Id { get; set; }
+        public string DRI2Name { get; set; }
+        public int RN1Id { get; set; }
+        public string RN1Name { get; set; }
+        public int RN2Id { get; set; }
+        public string RN2Name { get; set; }
+        public int RN3Id { get; set; }
+        public string RN3Name { get; set; }
+        public int CCA1Id { get; set; }
+        public string CCA1Name { get; set; }
+        public int CCA2Id { get; set; }
+        public string CCA2Name { get; set; }
+        public int CCA3Id { get; set; }
+        public string CCA3Name { get; set; }
+    }
+}
