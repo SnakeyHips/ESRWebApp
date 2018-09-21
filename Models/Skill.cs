b78ff983d8178ebed03968f3d95464df27a707b5
Jake Gough <super.jake@hotmail.co.uk>
2018-09-21 13:55:52 +0100

    Create Skill.cs

---
+++ Models/Skill.cs
@@ -0,0 +1,9 @@
+namespace ERSWebApp.Models
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
