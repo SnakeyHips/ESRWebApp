475c09acf27c64882cf1abe590a7aa3be07b5e70
Jake Gough <super.jake@hotmail.co.uk>
2018-09-21 13:04:15 +0100

    Update absence.ts

--- ClientApp/models/absence.ts
+++ ClientApp/models/absence.ts
@@ -1,9 +1,9 @@
 export interface Absence {
-	id: number,
-	staffId: number,
-	staffName: string,
-	type: string,
-	startDate: string,
-	endDate: string,
-	hours: number
+	id: number;
+	staffId: number;
+	staffName: string;
+	type: string;
+	startDate: string;
+	endDate: string;
+	hours: number;
 }
