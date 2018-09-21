33dd91449cf9b2dd99b654a5886ef1851ef712d2
Jake Gough <super.jake@hotmail.co.uk>
2018-09-21 13:03:40 +0100

    Update employee.ts

--- ClientApp/models/employee.ts
+++ ClientApp/models/employee.ts
@@ -1,18 +1,18 @@
 export interface Employee {
-	id: number,
-	name: string,
-	role: string,
-	skill: string,
-	address: string,
-	number: string,
-	contractHours: number,
-	appointedHours: number,
-	absenceHours: number,
-	lowRateUHours: number,
-	highRateUHours: number,
-	overtimeHours: number,
-	negHours: number,
-	coHours: number,
+	id: number;
+	name: string;
+	role: string;
+	skill: string;
+	address: string;
+	number: string;
+	contractHours: number;
+	appointedHours: number;
+	absenceHours: number;
+	lowRateUHours: number;
+	highRateUHours: number;
+	overtimeHours: number;
+	negHours: number;
+	coHours: number;
 	workPattern: string;
 	status: string;
 }
