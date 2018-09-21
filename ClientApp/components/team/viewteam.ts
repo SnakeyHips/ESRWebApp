abed9702cbd9cdbe1bb154afd566363d9edf577f
Jake Gough <super.jake@hotmail.co.uk>
2018-09-21 11:57:01 +0100

    Update viewteam.ts

--- ClientApp/components/team/viewteam.ts
+++ ClientApp/components/team/viewteam.ts
@@ -1,63 +1,37 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
-import { Session } from '../../models/session';
-import { Employee } from '../../models/employee';
+import { TeamSite } from '../../models/teamsite';
 
 @Component
 export default class ViewTeamComponent extends Vue {
-	sessions: Session[] = [];
+	teamsites: TeamSite[] = [];
 	startdate: string = "";
 	enddate: string = "";
 	loading: boolean = false;
 	search: string = "";
-	headers: object[] = [
-		{ text: 'Day', value: 'day' },
-		{ text: 'Date', value: 'date' },
-		{ text: 'Site', value: 'site' },
-		{ text: 'Time', value: 'time' },
-		{ text: 'Chairs', value: 'chairs' },
-		{ text: 'OCC', value: 'occ' },
-		{ text: 'Estimate', value: 'estimate' },
-	];
-
-	employee: Employee = {
-		id: 0,
-		name: "",
-		role: "",
-		skill: "",
-		address: "",
-		number: "",
-		contractHours: 0.0,
-		appointedHours: 0.0,
-		absenceHours: 0.0,
-		lowRateUHours: 0.0,
-		highRateUHours: 0.0,
-		overtimeHours: 0.0,
-		workPattern: "",
-		status: ""
-	}
-
-	mounted() {
-		this.loading = true;
-		fetch('api/Employee/GetById?id=' + this.$route.params.id)
-			.then(respone => respone.json() as Promise<Employee>)
-			.then(data => {
-				this.employee = data;
-				this.loading = false;
-			});
-	}
+	headers: object[] = [];
 
 	loadSessions() {
 		if (this.startdate != "" && this.enddate != "") {
 			if (!(new Date(this.enddate) < new Date(this.startdate))) {
 				this.loading = true;
-				console.log(this.startdate);
-				console.log(this.enddate);
-				fetch('api/Session/GetEmployeeSessions?staffid=' + this.employee.id + '&startdate=' + this.startdate + '&enddate=' + this.enddate)
-					.then(response => response.json() as Promise<Session[]>)
+				fetch('api/Team/GetTeamSites?id=' + this.$route.params.id + '&startdate=' + this.startdate + '&enddate=' + this.enddate)
+					.then(response => response.json() as Promise<TeamSite[]>)
 					.then(data => {
-						console.log(JSON.stringify(data));
-						this.sessions = data;
+						this.teamsites = data;
+						this.headers = [
+							{ text: 'Date', value: 'date' },
+							{ text: 'Day', value: 'day' },
+							{ text: this.teamsites[0].sV1Name, value: 'sV1Site' },
+							{ text: this.teamsites[0].drI1Name, value: 'drI1Site' },
+							{ text: this.teamsites[0].drI2Name, value: 'drI2Site' },
+							{ text: this.teamsites[0].rN1Name, value: 'rN1Site' },
+							{ text: this.teamsites[0].rN2Name, value: 'rN2Site' },
+							{ text: this.teamsites[0].rN3Name, value: 'rN3Site' },
+							{ text: this.teamsites[0].ccA1Name, value: 'ccA1Site' },
+							{ text: this.teamsites[0].ccA2Name, value: 'ccA2Site' },
+							{ text: this.teamsites[0].ccA3Name, value: 'ccA3Site' },
+						];
 						this.loading = false;
 					});
 			}
