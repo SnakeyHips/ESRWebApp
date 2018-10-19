da531a60b53d55d7c513b004c0e74fcfc73bd4bd
Jake Gough <super.jake@hotmail.co.uk>
2018-10-19 12:13:43 +0100

    Update app.ts

--- ClientApp/components/app/app.ts
+++ ClientApp/components/app/app.ts
@@ -15,7 +15,6 @@ export default class AppComponent extends Vue {
 	selecteddate: SelectedDate = new SelectedDate();
 
 	themeColour(colour: number) {
-		console.log(colour);
 		switch (colour) {
 			case 0:
 				this.$vuetify.theme.primary = "ED1C24";
