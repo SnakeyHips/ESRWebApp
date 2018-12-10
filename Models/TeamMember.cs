f69bf616b9aab15ebb54a239d11039a8c7076b6e
Jake Gough <super.jake@hotmail.co.uk>
2018-12-10 09:31:45 +0000

    Create TeamMember.cs

---
+++ Models/TeamMember.cs
@@ -0,0 +1,11 @@
+namespace ERSWebApp.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        public int TeamId { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeRole { get; set; }
+    }
+}
