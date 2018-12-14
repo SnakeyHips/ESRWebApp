10d7545fdabe1040b9641e4820a7b443308df21f
Jake Gough <super.jake@hotmail.co.uk>
2018-12-14 09:27:31 +0000

    Update Connection.cs

--- Connection.cs
+++ Connection.cs
@@ -2,6 +2,6 @@
 {
     public static class Connection
     {
-        public static string ConnString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Source\\ERSWebApp\\ERSWebApp\\ERSWebAppDB.mdf;Integrated Security=True";
+        public static string ConnString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Source\\ERSWebApp\\ERSWebAppDB.mdf;Integrated Security=True";
     }
 }
